0e972dec20b93a256fd511714a4a3c9e9800493b
tcruz1 <110926836+cruzjtyler@users.noreply.github.com>
2023-02-02 12:28:34 -0600

    Added backend folder

---
+++ BankingBE/src/main/java/com/tyler/BankingBeApplication.java
@@ -0,0 +1,13 @@
+package com.tyler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingBeApplication.class, args);
+	}
+
+}
---
+++ BankingBE/src/test/java/com/tyler/BankingBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tyler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
