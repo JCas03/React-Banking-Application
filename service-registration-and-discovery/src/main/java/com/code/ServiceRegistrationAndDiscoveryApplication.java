6c76fde6370b0a444e1e980e370737c139721609
Juan Castorena <03juan.c.castorena@gmail.com>
2023-02-17 12:03:56 -0600

    Added Eureka service and discovered userservice

---
+++ service-registration-and-discovery/src/main/java/com/code/ServiceRegistrationAndDiscoveryApplication.java
@@ -0,0 +1,15 @@
+package com.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceRegistrationAndDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistrationAndDiscoveryApplication.class, args);
+	}
+
+}
