feca57eeb9b5775a48ecd66c8829a85c28b8c876
tcruz1 <110926836+cruzjtyler@users.noreply.github.com>
2023-02-20 09:17:38 -0600

    Eureka implementation for Account Services

--- BankingBE/AccountMicroservice/src/main/java/com/code/AccountMicroservice/AccountMicroserviceApplication.java
+++ BankingBE/AccountMicroservice/src/main/java/com/code/AccountMicroservice/AccountMicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.code.AccountMicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AccountMicroserviceApplication {
 
 	public static void main(String[] args) {
