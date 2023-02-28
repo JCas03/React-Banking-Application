4c18bcbeda60f58c7d29d11637edac2f23aa9922
tcruz1 <110926836+cruzjtyler@users.noreply.github.com>
2023-02-28 16:59:48 -0600

    Account Service RabbitMQ

--- BankingBE/AccountMicroservice/src/main/java/com/code/AccountMicroservice/AccountMicroserviceApplication.java
+++ BankingBE/AccountMicroservice/src/main/java/com/code/AccountMicroservice/AccountMicroserviceApplication.java
@@ -1,15 +1,46 @@
 package com.code.AccountMicroservice;
 
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class AccountMicroserviceApplication {
 
+	@Value("${spring.rabbitmq.host}")
+	String host;
+	@Value("${spring.rabbitmq.username}")
+	String username;
+	@Value("${spring.rabbitmq.password}")
+	String password;
+
 	public static void main(String[] args) {
 		SpringApplication.run(AccountMicroserviceApplication.class, args);
 	}
 
+	@Bean
+	CachingConnectionFactory connectionFactory() {
+		CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory(host);
+		cachingConnectionFactory.setUsername(username);
+		cachingConnectionFactory.setPassword(password);
+		return cachingConnectionFactory;
+	}
+	@Bean
+	public MessageConverter jsonMessageConverter() {
+		return new Jackson2JsonMessageConverter();
+	}
+	@Bean
+	public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+		final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+		rabbitTemplate.setMessageConverter(jsonMessageConverter());
+		return rabbitTemplate;
+	}
 }
