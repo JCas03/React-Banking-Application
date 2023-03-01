f857a50f0787521d59d452c2a9870f26a252a516
Juan Castorena <03juan.c.castorena@gmail.com>
2023-03-01 10:28:40 -0600

    changes reciever

--- BankingBE/RMQReciever/src/main/java/com/code/config/RabbitMQConfig.java
+++ BankingBE/RMQReciever/src/main/java/com/code/config/RabbitMQConfig.java
@@ -1,10 +1,6 @@
 package com.code.config;
 
-
-
-
 import org.springframework.amqp.core.*;
-
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -16,26 +12,35 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
+
     @Value("${spring.rabbitmq.queue}")
     private String queue;
+
     @Value("${spring.rabbitmq.exchange}")
     private String exchange;
+
     @Value("${spring.rabbitmq.routingkey}")
     private String routingKey;
+
     @Value("${spring.rabbitmq.username}")
     private String username;
+
     @Value("${spring.rabbitmq.password}")
     private String password;
+
     @Value("${spring.rabbitmq.host}")
     private String host;
+
     @Bean
     Queue queue() {
         return new Queue(queue, true);
     }
+
     @Bean
     Exchange myExchange() {
         return ExchangeBuilder.directExchange(exchange).durable(true).build();
     }
+
     @Bean
     Binding binding() {
         return BindingBuilder
@@ -44,6 +49,7 @@ public class RabbitMQConfig {
                 .with(routingKey)
                 .noargs();
     }
+
     @Bean
     public ConnectionFactory connectionFactory() {
         CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory(host);
@@ -51,10 +57,12 @@ public class RabbitMQConfig {
         cachingConnectionFactory.setPassword(password);
         return cachingConnectionFactory;
     }
+
     @Bean
     public MessageConverter jsonMessageConverter() {
         return new Jackson2JsonMessageConverter();
     }
+
     @Bean
     public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
         final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
