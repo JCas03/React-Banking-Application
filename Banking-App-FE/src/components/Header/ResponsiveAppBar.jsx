93708f521dd13ca271ac32cf774852ecd6f45fdf
tcruz1 <110926836+cruzjtyler@users.noreply.github.com>
2023-03-09 11:58:07 -0600

    Navbar

---
+++ Banking-App-FE/src/components/Header/ResponsiveAppBar.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import {
+  AppBar,
+  Button,
+  Tab,
+  Tabs,
+  Toolbar,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';;
+import DrawerComp from "./Drawer";
+const Header = () => {
+  const [value, setValue] = useState();
+  const theme = useTheme();
+  console.log(theme);
+  const isMatch = useMediaQuery(theme.breakpoints.down("md"));
+  console.log(isMatch);
+
+  return (
+    <React.Fragment>
+      <AppBar sx={{ background: "#063970" }}>
+        <Toolbar>
+          <AccountBalanceIcon sx={{ transform: "scale(2)" }} />
+          {isMatch ? (
+            <>
+              <Typography sx={{ fontSize: "2rem", paddingLeft: "10%" }}>
+                Banking
+              </Typography>
+              <DrawerComp />
+            </>
+          ) : (
+            <>
+              <Tabs
+                sx={{ marginLeft: "auto" }}
+                indicatorColor="secondary"
+                textColor="inherit"
+                value={value}
+                onChange={(e, value) => setValue(value)}
+              >
+                <Tab label="Home" />
+                <Tab label="Account Services" />
+                <Tab label="Card Services" />
+                <Tab label="Contact" />
+              </Tabs>
+              <Button sx={{ marginLeft: "auto" }} variant="contained">
+                Login
+              </Button>
+              <Button sx={{ marginLeft: "10px" }} variant="contained">
+                SignUp
+              </Button>
+            </>
+          )}
+        </Toolbar>
+      </AppBar>
+    </React.Fragment>
+  );
+};
+
+export default Header;
