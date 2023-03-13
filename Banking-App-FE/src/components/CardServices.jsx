14d761185587c786dc28108ac2c3417d8e1ad6bb
tcruz1 <110926836+cruzjtyler@users.noreply.github.com>
2023-03-13 17:39:34 -0500

    Template for CC page, added button routing from home page

--- Banking-App-FE/src/components/CardServices.jsx
+++ Banking-App-FE/src/components/CardServices.jsx
@@ -1,11 +1,59 @@
 import React from 'react';
+import { Card, CardContent, Grid, Typography, CardActions } from "@mui/material";
+import { Box, Container } from "@mui/system";
+import Button from '@mui/material/Button';
 
-function CardServices() {
-    return(
-      <>
-        <h1> Card Services</h1>
-      </>
-    )
-    }
-    
-export default CardServices
+export default function CardServices() {
+  return (
+    <Box>
+      <Grid container spacing = {2}>
+        <Grid item xs={6}>
+          <Card sx={{ minWidth: 275 }}>
+            <CardContent>
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                CreditCard Name
+              </Typography>
+              <Typography variant="h5" component="div">
+                Card - Number
+              </Typography>
+              <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                Credit Balance
+              </Typography>
+              <Typography variant="body2">
+                Most Recent Transaction
+                <br />
+                Transaction ID: xxxxxxxxxxxx $123 at Place_Name
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">View All Transactions</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+        <Grid item xs={6}>
+          <Card sx={{ minWidth: 275 }}>
+            <CardContent>
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                CreditCard Name
+              </Typography>
+              <Typography variant="h5" component="div">
+                Card - Number
+              </Typography>
+              <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                Credit Balance
+              </Typography>
+              <Typography variant="body2">
+                Most Recent Transaction
+                <br />
+                Transaction ID: xxxxxxxxxxxx $123 at Place_Name
+              </Typography>
+            </CardContent>
+          <CardActions>
+            <Button size="small">View All Transactions</Button>
+          </CardActions>
+        </Card>
+      </Grid>
+    </Grid>
+  </Box>
+  );
+}
