55b20b33732bc5c5f355ca435658e180360dd3bc
kemet price <Kemet.Price1@gmail.com>
2023-03-16 12:47:35 -0400

    completed HomePage

--- Banking-App-FE/src/App.jsx
+++ Banking-App-FE/src/App.jsx
@@ -1,33 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import React from 'react'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import './App.css'
+import ResponsiveAppBar from './components/Header/ResponsiveAppBar'
+import Home from './components/Home'
+import Card from './components/CardServices'
+import AccountService from './components/AccountServices'
+import Contact from './components/ContactPage'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <BrowserRouter>
+      <ResponsiveAppBar />
+      <Routes>
+        <Route path="/" element = {<Home/>} />
+        <Route path="/card-services" element = {<Card/>} />
+        <Route path="/account-services" element = {<AccountService/>} />
+        <Route path="/contact" element = {<Contact/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ Banking-App-FE/src/components/Home.jsx
@@ -0,0 +1,152 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import MaterialLink from "@material-ui/core/Link";
+import { Link } from "react-router-dom";
+import { grey } from "@mui/material/colors";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <MaterialLink component={Link} to="/" color="inherit">
+        Binary Bank
+      </MaterialLink>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  "@global": {
+    a: {
+      textDecoration: "none",
+    },
+  },
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: grey,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+  },
+  cardMedia: {
+    paddingTop: "56.25%", // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: grey,
+    padding: theme.spacing(6),
+  },
+}));
+
+const cards = [1, 2, 3, 4];
+
+export default function Album() {
+  const classes = useStyles();
+
+  return (
+    <React.Fragment>
+      <main>
+        {/* Hero unit */}
+        <div className={classes.heroContent}>
+          <Container maxWidth="sm">
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              Welcome, User
+            </Typography>
+            <div className={classes.heroButtons}>
+              <Grid container spacing={2} justifyContent="center">
+                <Grid item>
+                  <Link to="/account-services">
+                    <Button variant="contained" color="primary">
+                      Account Dashboard
+                    </Button>
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link to="/card-services">
+                    <Button variant="outlined" color="primary">
+                      Credit Card Dashboard
+                    </Button>
+                  </Link>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+        <Container className={classes.cardGrid} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item xs={6}>
+                <Card sx={{ minWidth: 275 }}>
+                  <CardContent>
+                    <Typography
+                      sx={{ fontSize: 12 }}
+                      color="text.secondary"
+                      gutterBottom
+                    >
+                      Current Balance
+                    </Typography>
+                    <Typography variant="h4" component="div">
+                      $9xxx.xx
+                    </Typography>
+                    <Typography variant="body2">
+                      Account - Number
+                      <br />
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">View Account</Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+      {/* Footer */}
+      <footer className={classes.footer}>
+        <Typography variant="h6" align="center" gutterBottom></Typography>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="textSecondary"
+          component="p"
+        >
+          Thank you for choosing Binary Bank!
+        </Typography>
+        <Copyright />
+      </footer>
+      {/* End footer */}
+    </React.Fragment>
+  );
+}
