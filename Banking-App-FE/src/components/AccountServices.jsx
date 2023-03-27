cb9bd252601439a1d5d2138f537a191fdd1f660c
Juan Castorena <03juan.c.castorena@gmail.com>
2023-03-27 15:54:15 -0500

    new

--- Banking-App-FE/src/components/AccountServices.jsx
+++ Banking-App-FE/src/components/AccountServices.jsx
@@ -16,7 +16,7 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import "./css/AccountServices.css";
 import AccountService from "../services/AccountService";
-import { useState } from "react";
+import { useState, react } from "react";
 
 function createData(store, amount, address, date) {
   return { store, amount, address, date };
@@ -30,18 +30,15 @@ const rows = [
   createData("Louis Vutton", 600.0, "123 Louis Vutton Blv.", "08-16-2023"),
 ];
 export default function AccountServices() {
-  const [accName, setAccName] = useState("Loading...");
-  const [availableBalance, setAvailableBalance] = useState("Loading...");
-  const [accType, setAccType] = useState("Loading...");
+  const [accData, setAccData] = useState("Loading...");
 
 
   const loadAccounts = async () => {
     let accNum = 13;
-    const accData = await AccountService.getAccountByAccountNumber(accNum);
-    console.log(accData.data);
-    setAccName(accData.data.accountName);
-    setAccType(accData.data.accountType);
-    setAvailableBalance(accData.data.availableBalance);
+    let accsData = await AccountService.getAllAccountsByAccountNumber(accNum);
+    setAccData(accsData);
+    console.log(accData);
+  
   
     const transactionData = await AccountService.viewAllTransactionsOnAccount(
       accNum
@@ -50,41 +47,40 @@ export default function AccountServices() {
     let secLatestTransaction = transactionData.data.slice(1)[0];
     let thirdLatestTransaction = transactionData.data.slice(0)[0];
   
-    console.log(transactionData.data);
-    console.log(latestTransaction);
-    console.log(latestTransaction.accountNumber)
-    console.log(secLatestTransaction);
-    console.log(thirdLatestTransaction);
+    // console.log(transactionData.data);
+    // console.log(latestTransaction);
+    // console.log(latestTransaction.accountNumber)
+    // console.log(secLatestTransaction);
+    // console.log(thirdLatestTransaction);
   
-    console.log(accData.data);
-    console.log(accName);
-    console.log(accType);
-    console.log(availableBalance);
+
   };
   loadAccounts();
 
   return (
     <div>
       <Box>
-        <Grid container spacing={2}>
-          <Grid item xs={6}>
+        {/* <Grid container spacing={2}>
+          <Grid item xs={6}> */}
             <Card className="accountCard">
+              {accData.data?.map((user) =>( 
               <CardContent>
                 <Typography
                   className="maintext"
                   align="left"
                   variant="h2"
                   component="div"
-                >
-                  {accName}
+                alt="Loading...">
+                  {user.accountName}
                 </Typography>
                 <Typography
                   className="maintext"
                   gutterBottom
                   align="left"
                   variant="h3"
+                  alt="Loading..."
                 >
-                  ${availableBalance}
+                  ${user.availableBalance}
                 </Typography>
                 <TableContainer component={Paper}>
                   <Table>
@@ -111,6 +107,7 @@ export default function AccountServices() {
                   </Table>
                 </TableContainer>
               </CardContent>
+              ))}
               <Link
                 className="tlink"
                 underline="hover"
@@ -119,62 +116,8 @@ export default function AccountServices() {
                 See all Transactions
               </Link>
             </Card>
-          </Grid>
-
-          <Grid item xs={6}>
-            <Card className="accountCard">
-              <CardContent>
-                <Typography
-                  className="maintext"
-                  align="left"
-                  variant="h2"
-                  component="div"
-                >
-                  Savings Account
-                </Typography>
-                <Typography
-                  className="maintext"
-                  gutterBottom
-                  align="left"
-                  variant="h3"
-                >
-                  $9999.99
-                </Typography>
-                <TableContainer component={Paper}>
-                  <Table>
-                    <TableHead>
-                      <TableRow>
-                        <TableCell>Recent Transactions</TableCell>
-                        <TableCell align="right">Amount</TableCell>
-                        <TableCell align="right">Location</TableCell>
-                        <TableCell align="right">Date</TableCell>
-                      </TableRow>
-                    </TableHead>
-                    <TableBody>
-                      {rows.map((row) => (
-                        <TableRow key={row.store}>
-                          <TableCell component="th" scope="row">
-                            {row.store}
-                          </TableCell>
-                          <TableCell align="right">{row.amount}</TableCell>
-                          <TableCell align="right">{row.address}</TableCell>
-                          <TableCell align="right">{row.date}</TableCell>
-                        </TableRow>
-                      ))}
-                    </TableBody>
-                  </Table>
-                </TableContainer>
-              </CardContent>
-              <Link
-                className="tlink"
-                underline="hover"
-                href="/all-transactions"
-              >
-                See all Transactions
-              </Link>
-            </Card>
-          </Grid>
-        </Grid>
+          {/* </Grid>
+        </Grid> */}
       </Box>
     </div>
   );
