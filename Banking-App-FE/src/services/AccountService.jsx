cc087e41abf9c47819df3e4b043fcaf6f9cfa114
Juan Castorena <03juan.c.castorena@gmail.com>
2023-05-04 13:01:08 -0500

    adds

--- Banking-App-FE/src/services/AccountService.jsx
+++ Banking-App-FE/src/services/AccountService.jsx
@@ -1,41 +1,52 @@
-
 import axios from "axios";
 
 class AccountService {
+  getAllAccounts() {
+    return axios.get("http://localhost:8080/accms/accounts");
+  }
 
-    getAllAccounts(){
-        return axios.get("http://localhost:8080/accms/accounts");
-    }
+  createNewAccount(account) {
+    return axios.post("http://localhost:8080/accms/new-account", account);
+  }
 
-    createNewAccount(account){
-        return axios.post("http://localhost:8080/accms/new-account", account);
-    }
+  getAccountByAccountNumber(accountNumber) {
+    return axios.get(`http://localhost:8080/accms/account/${accountNumber}`);
+  }
 
-    getAccountByAccountNumber(accountNumber) {
-        return axios.get(`http://localhost:8080/accms/account/${accountNumber}`);
-    }
+  getAllAccountsByAccountNumber(accountNumber) {
+    return axios.get(
+      `http://localhost:8080/accms/all-accounts/${accountNumber}`
+    );
+  }
+  deposit(accountNumber, transaction) {
+    return axios.post(`http://localhost:8080/accms/deposit/${accountNumber}`, {
+      depositAmount: transaction,
+    });
+  }
 
-    getAllAccountsByAccountNumber(accountNumber) {
-        return axios.get(`http://localhost:8080/accms/all-accounts/${accountNumber}`)
-    }
-    deposit(accountNumber, transaction) {
-        return axios.post(`http://localhost:8080/accms/deposit/${accountNumber}`, transaction);
-    }
+  withdrawal(accountNumber, transaction) {
+    return axios.post(
+      `http://localhost:8080/accms/withdrawal/${accountNumber}`,
+      { withdrawalAmount: transaction }
+    );
+  }
 
-    withdrawal(accountNumber, transaction) {
-        return axios.post(`http://localhost:8080/accms/withdrawal/${accountNumber}`, transaction);
-    }
+  transferFunds(fromAccountNumber, toAccountNumber, transaction) {
+    return axios.post(
+      `http://localhost:8080/accms/transfer/${fromAccountNumber}/${toAccountNumber}`,{transferAmount: transaction}
+    );
+  }
 
-    transferFunds(fromAccountNumber, toAccountNumber, transaction) {
-        return axios.post(`http://localhost:8080/accms/transfer/${fromAccountNumber}/${toAccountNumber}`, transaction);
-    }
+  viewAllTransactionsOnAccount(accountNumber) {
+    return axios.get(
+      `http://localhost:8080/accms/view-transactions/${accountNumber}`
+    );
+  }
 
-    viewAllTransactionsOnAccount(accountNumber) {
-        return axios.get(`http://localhost:8080/accms/view-transactions/${accountNumber}`);
-    }
-
-    getAllAccountsByUsername(username) {
-        return axios.get(`http://localhost:8080/accms/all-accounts-by-username/${username}`);
-    }
+  getAllAccountsByUsername(username) {
+    return axios.get(
+      `http://localhost:8080/accms/all-accounts-by-username/${username}`
+    );
+  }
 }
 export default new AccountService();
